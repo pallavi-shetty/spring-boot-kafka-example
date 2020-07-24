d89404eab4035a1a4780e2b6e978ea249b6274a2
pallavi-shetty <pallavis.ttl@tatamotors.com>
2020-07-24 17:49:38 +0530

    first commit

---
+++ src/main/java/com/example/AppBeans.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppBeans {
+
+    @Bean
+    public SpringBootKafkaProducer initProducer() {
+        return new SpringBootKafkaProducer();
+    }
+}
